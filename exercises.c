e3b7a186a5434b4fc1dd498e9270cbf3c685ee8f
Your Name <you@example.com>
2025-03-19 09:07:57 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -85,7 +85,7 @@ void eliminaElementos(List*L, int elem){
    int* dato = first(L);
    while(dato != NULL)
    {
-      if(dato == elem)
+      if(*dato == elem) //puntero es cmo acceder al dato que apunta el dato
       {
          popCurrent(L);
       }
